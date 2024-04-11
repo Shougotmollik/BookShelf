ba878ca1f81553e77c22b60131c28ba1dd19a95c
Shougot mollik <shougotmollik@gmail.com>
2024-04-11 11:31:23 +0600

    my button widget

---
+++ lib/widgets/my_button.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  const PrimaryButton(
+      {super.key, required this.btnText, required this.btnOnPress});
+
+  final String btnText;
+  final VoidCallback btnOnPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: btnOnPress,
+      child: Container(
+        height: 60,
+        decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.primary,
+          borderRadius: BorderRadius.circular(12),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              btnText,
+              style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                    color: Theme.of(context).colorScheme.onPrimary,
+                  ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
