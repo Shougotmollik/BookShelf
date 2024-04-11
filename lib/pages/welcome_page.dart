bfc71e6e158c6f3dd399855aaa882e1f59059fb7
Shougot mollik <shougotmollik@gmail.com>
2024-04-11 11:31:33 +0600

    welcome page

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -1,7 +1,6 @@
 import 'package:book_shelf/config/image_strings.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:book_shelf/widgets/my_button.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class WelcomePage extends StatelessWidget {
   const WelcomePage({super.key});
@@ -9,101 +8,91 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.all(8.0),
-          height: 550,
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.primary,
-            borderRadius: const BorderRadius.only(
-              bottomLeft: Radius.circular(18),
-              bottomRight: Radius.circular(18),
+      body: Column(
+        children: [
+          Container(
+            padding: const EdgeInsets.all(8.0),
+            height: 550,
+            decoration: BoxDecoration(
+              color: Theme.of(context).colorScheme.primary,
+              borderRadius: const BorderRadius.only(
+                bottomLeft: Radius.circular(18),
+                bottomRight: Radius.circular(18),
+              ),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Expanded(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset(
+                        AppImageStrings.bookImage,
+                        width: 380,
+                      ),
+                      const SizedBox(height: 40),
+                      Text(
+                        "E-Book Shelf",
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context)
+                            .textTheme
+                            .headlineLarge
+                            ?.copyWith(
+                                color: Theme.of(context).colorScheme.onPrimary),
+                      ),
+                      const SizedBox(height: 25),
+                      Text(
+                        "Here you can find best book for you and you can also read book and listens book ",
+                        textAlign: TextAlign.center,
+                        style: Theme.of(context).textTheme.labelSmall?.copyWith(
+                            color: Theme.of(context).colorScheme.onPrimary),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Row(
                   children: [
-                    Image.asset(
-                      AppImageStrings.bookImage,
-                      width: 380,
-                    ),
-                    const SizedBox(height: 40),
                     Text(
-                      "E-Book Shelf",
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context)
-                          .textTheme
-                          .headlineLarge
-                          ?.copyWith(
-                              color: Theme.of(context).colorScheme.onPrimary),
-                    ),
-                    const SizedBox(height: 25),
-                    Text(
-                      "Here you can find best book for you and you can also read book and listens book ",
-                      textAlign: TextAlign.center,
-                      style: Theme.of(context).textTheme.labelSmall?.copyWith(
-                          color: Theme.of(context).colorScheme.onPrimary),
+                      "Disclaimer",
+                      style: Theme.of(context).textTheme.labelLarge,
+                    )
+                  ],
+                ),
+                const SizedBox(height: 10),
+                Row(
+                  children: [
+                    Flexible(
+                      child: Text(
+                        "n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum",
+                        textAlign: TextAlign.start,
+                        style: Theme.of(context).textTheme.labelSmall,
+                      ),
                     ),
                   ],
                 ),
-              )
-            ],
+              ],
+            ),
           ),
-        ),
-        const SizedBox(height: 30),
-        Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                children: [
-                  Text(
-                    "Disclaimer",
-                    style: Theme.of(context).textTheme.labelLarge,
-                  )
-                ],
-              ),
-              const SizedBox(height: 10),
-              Row(
-                children: [
-                  Flexible(
-                    child: Text(
-                      "n publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsum",
-                      textAlign: TextAlign.start,
-                      style: Theme.of(context).textTheme.labelSmall,
-                    ),
-                  ),
-                ],
-              ),
-              const SizedBox(height: 60),
-              Container(
-                height: 60,
-                decoration: BoxDecoration(
-                  color: Theme.of(context).colorScheme.primary,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Continue",
-                      style: Theme.of(context).textTheme.bodyLarge?.copyWith(
-                            color: Theme.of(context).colorScheme.onPrimary,
-                          ),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+          const Spacer(),
+          Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: PrimaryButton(
+              btnOnPress: () {},
+              btnText: 'Continue',
+            ),
           ),
-        ),
-      ],
-    ));
+        ],
+      ),
+    );
   }
 }
