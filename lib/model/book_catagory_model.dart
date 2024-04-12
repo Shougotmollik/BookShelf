607555720e2c739e7ba2ef8d2087f9aaa4818809
Shougot mollik <shougotmollik@gmail.com>
2024-04-12 15:07:58 +0600

    book catagory model

---
+++ lib/model/book_catagory_model.dart
@@ -0,0 +1,24 @@
+import 'package:book_shelf/config/image_strings.dart';
+
+var bookcatagory = [
+  {
+    "icon": AppImageStrings.heartIcon,
+    "label": "Romance",
+  },
+  {
+    "icon": AppImageStrings.worldIcon,
+    "label": "Documentry",
+  },
+  {
+    "icon": AppImageStrings.planeIcon,
+    "label": "Travel",
+  },
+  {
+    "icon": AppImageStrings.starIcon,
+    "label": "Fantacy",
+  },
+  {
+    "icon": AppImageStrings.worldIcon,
+    "label": "Motivation",
+  },
+];
