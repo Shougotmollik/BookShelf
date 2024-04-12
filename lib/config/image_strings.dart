1d61e1502f9dfb7df02665f847535e81ae463155
Shougot mollik <shougotmollik@gmail.com>
2024-04-12 13:27:33 +0600

    image strings

--- lib/config/image_strings.dart
+++ lib/config/image_strings.dart
@@ -1,6 +1,20 @@
 class AppImageStrings {
   AppImageStrings._();
 
-//   images
+  //?   images
   static const bookImage = "assets/icons/books.png";
+
+  //? Icons
+
+  static const drawerIcon = "assets/icons/drawer_icon.svg";
+  static const deleteIcon = "assets/icons/delete_icon.svg";
+  static const backIcon = "assets/icons/back.svg";
+  static const googleIcon = "assets/icons/google_icon.svg";
+  static const heartIcon = "assets/icons/heart.svg";
+  static const planeIcon = "assets/icons/plane.svg";
+  static const playIcon = "assets/icons/play.svg";
+  static const searchIcon = "assets/icons/search_icon.svg";
+  static const starIcon = "assets/icons/star.svg";
+  static const uploadIcon = "assets/icons/upload_icon.svg";
+  static const worldIcon = "assets/icons/world.svg";
 }
