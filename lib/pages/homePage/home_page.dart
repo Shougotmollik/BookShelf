290ca8990d49f436a3141edc60517acb84146c2d
Shougot mollik <shougotmollik@gmail.com>
2024-04-13 14:56:16 +0600

    home page

--- lib/pages/homePage/home_page.dart
+++ lib/pages/homePage/home_page.dart
@@ -1,6 +1,9 @@
-import 'package:book_shelf/model/book_catagory_model.dart';
+import 'package:book_shelf/model/book_category_model.dart';
+import 'package:book_shelf/model/book_data.dart';
 import 'package:book_shelf/pages/homePage/app_bar.dart';
-import 'package:book_shelf/widgets/catagory_tab_bar.dart';
+import 'package:book_shelf/widgets/book_card.dart';
+import 'package:book_shelf/widgets/book_list_card.dart';
+import 'package:book_shelf/widgets/category_tab_bar.dart';
 import 'package:book_shelf/widgets/my_input_text_filed.dart';
 import 'package:flutter/material.dart';
 
@@ -10,96 +13,147 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Container(
-            padding: const EdgeInsets.all(8.0),
-            height: 350,
-            decoration: BoxDecoration(
-                color: Theme.of(context).colorScheme.primary,
-                borderRadius: const BorderRadius.only(
-                  bottomRight: Radius.circular(12),
-                  bottomLeft: Radius.circular(12),
-                )),
-            child: Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    child: Column(
-                      children: [
-                        const SizedBox(height: 40),
-                        const HomeAppBar(),
-                        Row(
-                          children: [
-                            Text("Good Morning 😃, ",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyMedium
-                                    ?.copyWith(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .onPrimary)),
-                            Text("Shougot",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .bodyLarge
-                                    ?.copyWith(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .onPrimary)),
-                          ],
-                        ),
-                        const SizedBox(height: 5),
-                        Row(
-                          children: [
-                            Text(
-                              "Time to read book and enhance your knowledge",
-                              style: Theme.of(context)
-                                  .textTheme
-                                  .labelSmall
-                                  ?.copyWith(
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .onPrimary),
-                            )
-                          ],
-                        ),
-                        const SizedBox(height: 20),
-                        const MyInputTextField(),
-                        const SizedBox(height: 20),
-                        Row(
-                          children: [
-                            Text("Topics",
-                                style: Theme.of(context)
-                                    .textTheme
-                                    .labelLarge
-                                    ?.copyWith(
-                                        color: Theme.of(context)
-                                            .colorScheme
-                                            .onPrimaryContainer))
-                          ],
-                        ),
-                        const SizedBox(height: 15),
-                        SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: Row(
-                            children: bookcatagory
-                                .map(
-                                  (e) => CatagoryTabBar(
-                                      iconPath: e["icon"]!,
-                                      btnText: e["label"]!),
-                                )
-                                .toList(),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.all(8.0),
+              height: 350,
+              decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.primary,
+                  borderRadius: const BorderRadius.only(
+                    bottomRight: Radius.circular(12),
+                    bottomLeft: Radius.circular(12),
+                  )),
+              child: Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 40),
+                          const HomeAppBar(),
+                          Row(
+                            children: [
+                              Text("Good Morning 😃, ",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyMedium
+                                      ?.copyWith(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .onPrimary)),
+                              Text("Shougot",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge
+                                      ?.copyWith(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .onPrimary)),
+                            ],
                           ),
-                        )
-                      ],
+                          const SizedBox(height: 5),
+                          Row(
+                            children: [
+                              Text(
+                                "Time to read book and enhance your knowledge",
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelSmall
+                                    ?.copyWith(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onPrimary),
+                              )
+                            ],
+                          ),
+                          const SizedBox(height: 20),
+                          const MyInputTextField(),
+                          const SizedBox(height: 15),
+                          Row(
+                            children: [
+                              Text("Topics",
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .labelLarge
+                                      ?.copyWith(
+                                          color: Theme.of(context)
+                                              .colorScheme
+                                              .onPrimaryContainer))
+                            ],
+                          ),
+                          const SizedBox(height: 10),
+                          SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: Row(
+                              children: bookcatagory
+                                  .map(
+                                    (e) => CatagoryTabBar(
+                                        iconPath: e["icon"]!,
+                                        btnText: e["label"]!),
+                                  )
+                                  .toList(),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(height: 10.0),
+            Padding(
+              padding: const EdgeInsets.all(12.0),
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      Text('Trending',
+                          style: Theme.of(context).textTheme.labelMedium),
+                    ],
+                  ),
+                  const SizedBox(height: 10.0),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: bookData
+                          .map((e) => MyBookCard(
+                                coverImage: e.coverUrl!,
+                                titleText: e.title!,
+                                onTap: () {},
+                              ))
+                          .toList(),
                     ),
+                  ),
+                  const SizedBox(height: 10),
+                  Row(
+                    children: [
+                      Text(
+                        "Your Interests",
+                        style: Theme.of(context).textTheme.labelMedium,
+                      )
+                    ],
+                  ),
+                  Column(
+                    children: bookData
+                        .map((e) => MyBookListCart(
+                              title: e.title!,
+                              coverUrl: e.coverUrl!,
+                              author: e.author!,
+                              price: e.price!,
+                              rating: e.rating!,
+                              totalRating: e.numberOfRating!,
+                            ))
+                        .toList(),
                   )
                 ],
               ),
             ),
-          )
-        ],
+          ],
+        ),
       ),
     );
   }
